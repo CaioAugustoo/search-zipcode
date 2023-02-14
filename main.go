c1ee6796f8a8541b26594be595c966e128e1449e
caioaugustoo <caioamfr@gmail.com>
2023-02-14 12:53:36 -0300

    feat: use net/http to fetch address from an external api

--- main.go
+++ main.go
@@ -3,39 +3,67 @@ package main
 import (
 	"io"
 	"net/http"
-	"os"
 )
 
-func getRequestURL(zipcode string) string {
-	return "https://viacep.com.br/ws/" + zipcode + "/json/"
+type Address struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func getAddress(zipcode string) ([]byte, error) {
+	url := "https://viacep.com.br/ws/" + zipcode + "/json/"
+	res, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(404)
+		return
+	}
+
+	zipcode := r.URL.Query().Get("code")
+
+	if zipcode == "" {
+		w.WriteHeader(400)
+		return
+	}
+
+	data, err := getAddress(zipcode)
+
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+
+	w.Write(data)
 }
 
 func main() {
-	for _, v := range os.Args[1:] {
-		url := getRequestURL(v)
-
-		res, err := http.Get(url)
-
-		if err != nil {
-			panic(err)
-		}
-
-		defer res.Body.Close()
-
-		data, err := io.ReadAll(res.Body)
-
-		if err != nil {
-			panic(err)
-		}
-
-		file, err := os.Create("result.json")
-
-		if err != nil {
-			panic(err)
-		}
-
-		file.WriteString(string(data))
-
-		defer file.Close()
-	}
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", nil)
 }
